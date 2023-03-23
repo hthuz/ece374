dfdc7b6113e55907b6d8c5a80bb611ec527ce8aa
Autentico <hanggang.20@intl.zju.edu.cn>
2023-03-23 19:18:53 +0800

    Update: hanoi0 function succeeds

--- hw5/hanoi.py
+++ hw5/hanoi.py
@@ -1,10 +1,10 @@
 
 
-n = 2
+n = 3
 initpeg = [i for i in range(n , 0, -1)]
 
 # Leftmost item in list means bottom of the peg
-pegs = [[], initpeg, []]
+pegs = [[],initpeg, []]
 
 
 
@@ -25,6 +25,28 @@ def hanoi(n, src, dest, tmp):
 
 
 def hanoi0(n,src,dest,tmp):
+
+    if (n == 0):
+        return
+
+    if (n > 0):
+
+        hanoi0(n - 1, src, dest, tmp)
+
+        disk = pegs[src][-1]
+        pegs[src].pop()
+        pegs[tmp].append(disk)
+        print(pegs)
+
+        hanoi0(n - 1, dest, src, tmp)
+
+        disk = pegs[tmp][-1]
+        pegs[tmp].pop()
+        pegs[dest].append(disk)
+        print(pegs)
+
+        hanoi0(n - 1,src, dest, tmp )
+
     return
 
         
@@ -34,7 +56,8 @@ def hanoi0(n,src,dest,tmp):
 if __name__ == "__main__":
 
     print(pegs)
-    # print(pegs)
+    hanoi0(n,1,2,0)
+    print(pegs)
 
 
 
