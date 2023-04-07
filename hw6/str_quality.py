4abcbe3c20b7a33a6aff094c8b4aa4495937b165
hthuz <hanggang.20@intl.zju.edu.cn>
2023-04-08 00:06:39 +0800

    Add: str quality different implement

--- hw6/str_quality.py
+++ hw6/str_quality.py
@@ -11,9 +11,9 @@ def q(i,j):
     if inputstr[i: j] == "DOG":
         return 10
     if inputstr[i: j] == "ATDO":
-        return 15
+        return 11
     if inputstr[i: j] == "CATD":
-        return 20
+        return 13
     if inputstr[i: j] == "C":
         return 13
     if inputstr[i: j] == "A":
@@ -29,7 +29,17 @@ def hqs(i,j,x):
     else:
         return max(hqs(i + 1, j, x), hqs(i, j - 1, x))
 
+def hqs2(i,j,x):
+    if (i == j):
+        return x
+
+    m = max(hqs2(i + 1, j, x), hqs2(i, j - 1, x))
+    if q(i, j) >x:
+        m = max(m, q(i,j))
+    return m
+
 if __name__ == "__main__":
 
     print(hqs(0, n, 0))
+    print(hqs2(0, n, 0))
 
